a110bb6126fdfbd34c0c078ad399ef6d0c5740ae
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-23 16:58:40 +0200

    Integrate lombok

--- src/main/java/de/neuefische/stream/schulung/Student.java
+++ src/main/java/de/neuefische/stream/schulung/Student.java
@@ -1,47 +1,18 @@
 package de.neuefische.stream.schulung;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Student {
 
     private String name;
     private String postalCode;
     private String age;
 
-    public Student(String name, String number, String age) {
-        this.name = name;
-        this.postalCode = number;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", age='" + age + '\'' +
-                '}';
-    }
 }
